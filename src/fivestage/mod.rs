d9ea7f8bf31768d4e496e516abd2fd0364f03f63
Jimmy Ostler <jtostler1@gmail.com>
2026-04-03 19:59:22 -0700

    attempt 1

--- src/fivestage/mod.rs
+++ src/fivestage/mod.rs
@@ -1,6 +1,7 @@
 use crate::{common::*, instr::decoding::{RV32I, Reg}};
 
 // 5 stage RISC pipeline
+// 
 // 1. Fetch
 // 2. Decode
 // 3. Execution
@@ -9,22 +10,39 @@ use crate::{common::*, instr::decoding::{RV32I, Reg}};
 
 pub struct Machine<const MEM_SIZE: usize> {
     registers: [Register; 32],
-    memory: Box<[u8; MEM_SIZE]>,
-
-    // Pipeline specific values
-    fetched: Option<u32>,
-    decoded: Option<RV32I>,
-    // Computational value from execute stage
-    value: Option<u32>,
-    mem: Option<MemoryAccess>,
-    writeback: Option<(u32, RV32I)>,
+    memory:    Box<[u8; MEM_SIZE]>,
+    pc: usize,
+    pipleline: Pipeline
 }
 
-enum AccType {
-    Read,
-    Write,
+// Kinda weird way of trying to do this, but maybe it'll work?
+
+struct Pipeline {
+    // Represents a list of current registers in the pipeline, and at what spot.
+    // This allows us to add hazards to avoid incoherence.
+    dependencies: Vec<Reg>,
+    // During decode, these are set 
+    vals1: u32,
+    vals2: u32,
+    // set during execute. For branches, this is set to the next PC. Branches add hazards and stop the pipeline until they can
+    // "retire".
+    valres: u32,
+    // set during memory access. res1 is used for the WB stage
+    valmem: u32,
+    valres1: u32,
+
+    fetch:     Vec<Option<u32>>,
+    decode:    Vec<Option<RV32I>>,
+    // u32 for reg WB, opt for PC writeback
+    value:     Vec<Option<(u32, Option<u32>)>>,
+    memory:    Vec<Option<MemoryAccess>>,
+    writeback: Vec<Option<WriteBack>>,
 }
 
-struct MemoryAccess(AccType, Reg, u32);
+enum MemoryAccess {
+    Read(Reg),
+    Write(Reg),
+    None,
+}
 
-struct WriteBack(Reg, u32);
+struct WriteBack(Reg);
